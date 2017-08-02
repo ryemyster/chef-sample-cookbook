04ade6d51aee0a61008bdf0a026f0c90c10ddbcf
Ryan <ryan@ibbs-MacBook-Air.local>
2017-08-02 09:31:04 -0600

    update

--- Berksfile
+++ Berksfile
@@ -3,3 +3,4 @@ source 'https://supermarket.chef.io'
 
 metadata
 cookbook 'nodejs', '~> 4.0.0'
+cookbook 'git', '~> 6.1.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,11 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 # Cookbook Name'default.rb'
-
 include_recipe "nodejs::nodejs_from_binary"
 include_recipe "nodejs::npm"
+include_recipe "git"
 
 #configure required wrappings
 log "Sample Log Output for Eyeballing Random Stuff" do
   level :debug
+end
+
+log "Install git" do
+  level :debug
+end
+
+git_client 'default' do
+  action :install
 end
